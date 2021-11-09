b2ecdf79369f8aad6eb8724f14e78a52922bef65
tuanit168 <tuanit168@gmail.com>
2021-11-09 22:48:32 +1100

    Defining a data product model

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
