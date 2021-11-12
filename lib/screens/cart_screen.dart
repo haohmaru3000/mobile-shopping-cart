d063e3f525eb5433e222aa0d3b691bea2dbc6f4e
tuanit168 <tuanit168@gmail.com>
2021-11-13 00:27:37 +1100

    Pressing Order-Now button to clear cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../providers/orders.dart';
 import '../providers/cart.dart'
     show Cart; // Only import class Cart, not CartItem in cart.dart
 import '../widgets/cart_item.dart';
@@ -39,7 +40,13 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).colorScheme.primary,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear(); // Only listen to changes in "cart", not in "orders"
+                    },
                     child: const Text('ORDER NOW'),
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all(
