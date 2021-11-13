d912821b6a756acd43a85cd4c24617d5e8bde0cf
tuanit168 <tuanit168@gmail.com>
2021-11-13 20:57:54 +1100

    Initializing 'Edit_Product_Screen' page

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+/*
+ *  Form inputs are only good for a widget in local state => use Stateful widget
+ *  (what the users entered is important for this widget because we wanna validate it, and store there.
+ *   Once user submits like pressing submit button, we typically wanna save that info into the App-wide state)
+ *   EX: create a product, signup a users, whatever we're doing but until the submit button is pressed 
+ *      we only wanna only manage that data in our local widget as users might cancel adding, might close the app...
+ *      => So, the general app is not affected by the user input until it's really submitted
+ *      => We want to manage the User input and validate it and so on, locally in this widget
+ */
+class EditProductScreen extends StatefulWidget {
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+/* The state which is related to EditProductScreen widget */
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // Standalone page
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: null,
+    );
+  }
+}
