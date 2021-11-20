a241cc5ddc177fca103e3535473fc7fb3e8303ea
tuanit168 <tuanit168@gmail.com>
2021-11-20 16:10:55 +1100

    Storing orders in the web server

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -39,21 +39,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).colorScheme.primary,
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      cart.clear(); // Only listen to changes in "cart", not in "orders"
-                    },
-                    child: const Text('ORDER NOW'),
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all(
-                        Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                  ),
+                  OrderButton(cart: cart),
                 ],
               ),
             ),
@@ -75,6 +61,51 @@ class CartScreen extends StatelessWidget {
             itemCount: cart.items.length,
           ))
         ],
+      ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+              await Provider.of<Orders>(context, listen: false).addOrder(
+                widget.cart.items.values.toList(),
+                widget.cart.totalAmount,
+              );
+              setState(() {
+                _isLoading = false;
+              });
+              widget.cart
+                  .clear(); // Only listen to changes in "cart", not in "orders"
+            },
+      child: _isLoading
+          ? const CircularProgressIndicator()
+          : const Text('ORDER NOW'),
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all(
+          Theme.of(context).colorScheme.primary,
+        ),
       ),
     );
   }
