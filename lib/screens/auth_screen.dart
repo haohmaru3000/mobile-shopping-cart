0ee9e381ccff0111d6d5693435f2b191e3307ae0
tuanit168 <tuanit168@gmail.com>
2021-11-21 23:54:46 +1100

    Implemented Login function

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -112,6 +112,10 @@ class _AuthCardState extends State<AuthCard> {
     });
     if (_authMode == AuthMode.Login) {
       // Log user in
+      await Provider.of<Auth>(context, listen: false).login(
+        _authData['email'].toString(),
+        _authData['password'].toString(),
+      );
     } else {
       // Sign user up
       await Provider.of<Auth>(context, listen: false).signup(
